bd44b58f963703c337745af462b855fa5c3289c0
RawJrk <rodjer7777@gmail.com>
2025-05-31 22:45:01 +0300

    Removed area/category from cards

--- front/src/components/RecipesList/card.tsx
+++ front/src/components/RecipesList/card.tsx
@@ -26,9 +26,6 @@ export default function RecipeCard({ data: recipe }: IRecipeCardProps) {
             {recipe.title}
           </h5>
         </Link>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {recipe.area}, {recipe.category}
-        </p>
         <Link
           href={infoHref}
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
