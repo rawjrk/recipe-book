00a2946d41383dd063efb617f3cab3bbb07b5426
RawJrk <rodjer7777@gmail.com>
2025-05-31 18:40:56 +0300

    Added local API calls

---
+++ front/src/api/recipes.ts
@@ -0,0 +1,18 @@
+import { IRecipe, IRecipeInfo } from "./types";
+
+const { API_DOMAIN } = process.env;
+
+export async function getRecipes(): Promise<IRecipe[]> {
+  // TODO: add filter options
+  const res = await fetch(`${API_DOMAIN}/recipes`);
+  const json = await res.json();
+
+  return json;
+}
+
+export async function getRecipeInfo(id: number): Promise<IRecipeInfo> {
+  const res = await fetch(`${API_DOMAIN}/recipe/${id}`);
+  const json = await res.json();
+
+  return json;
+}
