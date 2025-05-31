acffb7202db1d7471db8c951911cfbbfb4fd5fc6
RawJrk <rodjer7777@gmail.com>
2025-05-31 20:02:08 +0300

    Added listing-related api calls

---
+++ front/src/api/listing.ts
@@ -0,0 +1,34 @@
+const { API_DOMAIN } = process.env;
+
+export async function getCategories(): Promise<string[]> {
+  const res = await fetch(`${API_DOMAIN}/categories`);
+  const json = await res.json();
+
+  if (!res.ok) {
+    throw new ApiError(res.status, json?.message);
+  }
+
+  return json;
+}
+
+export async function getAreas(): Promise<string[]> {
+  const res = await fetch(`${API_DOMAIN}/areas`);
+  const json = await res.json();
+
+  if (!res.ok) {
+    throw new ApiError(res.status, json?.message);
+  }
+
+  return json;
+}
+
+export async function getIngredients(): Promise<string[]> {
+  const res = await fetch(`${API_DOMAIN}/ingredients`);
+  const json = await res.json();
+
+  if (!res.ok) {
+    throw new ApiError(res.status, json?.message);
+  }
+
+  return json;
+}
