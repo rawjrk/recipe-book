b70015c877f737e305a41df2fd7c7bbe084734c6
RawJrk <rodjer7777@gmail.com>
2025-06-01 01:48:51 +0300

    Refactoring RecipeInfo layout

--- front/src/components/RecipeInfo/index.tsx
+++ front/src/components/RecipeInfo/index.tsx
@@ -1,47 +1,69 @@
-import React from "react";
+"use server";
+
 import { IRecipeInfo } from "@/api/types";
 import Link from "next/link";
 import Image from "next/image";
+import { getRecipes } from "@/api/recipes";
 
 type IRecipeInfoProps = {
   data: IRecipeInfo;
 };
 
-export default function RecipeInfo({ data: recipe }: IRecipeInfoProps) {
+export default async function RecipeInfo({ data: recipe }: IRecipeInfoProps) {
+  const { category } = recipe;
+  const recipiesFromCategory = await getRecipes({ category }).catch(() => []);
+
   return (
-    <div className="rounded-lg shadow-md p-6 max-w-3xl mx-auto">
-      <Image
-        src={recipe.thumbnail}
-        alt={recipe.title}
-        width={600}
-        height={400}
-        className="rounded-md mb-4"
-      />
-      <h2 className="text-2xl font-bold mb-2">{recipe.title}</h2>
-      <div className="text-sm text-gray-400 mb-4">
-        <span>
-          Area: <Link href={`/recipes?area=${recipe.area}`}>{recipe.area}</Link>
-        </span>
+    <div className="grid grid-cols-1 lg:grid-cols-3">
+      <div className="flex flex-col m-4">
+        <Image
+          src={`${recipe.thumbnail}`}
+          alt={recipe.title}
+          width={500}
+          height={500}
+          className="rounded-lg lg:sticky top-0 self-center"
+        />
       </div>
 
-      <h3 className="text-lg font-semibold mb-2">Instructions</h3>
-      <div>
-        {recipe.instructions.split("\n").map((line, index) => (
-          <p key={index} className="mb-2">
-            {line}
-          </p>
-        ))}
-      </div>
+      <main className="m-4 flex flex-col">
+        <h2 className="text-3xl font-bold mb-2 self-center">{recipe.title}</h2>
+        <div className="text-2x1 mb-2 self-center">
+          <span className="text-gray-400 ">Area:</span>{" "}
+          <Link href={`/recipes?area=${recipe.area}`}>{recipe.area}</Link>
+        </div>
 
-      <h3 className="text-lg font-semibold mb-2">Ingredients</h3>
-      <ul className="list-disc list-inside mb-4">
-        {recipe.ingredients.map((ing, index) => (
-          <li key={index}>
-            <Link href={`/recipes?ingredient=${ing.name}`}>{ing.name}</Link>
-            <span className="text-gray-400"> — {ing.measure}</span>
-          </li>
-        ))}
-      </ul>
+        <h3 className="text-lg font-semibold mb-2">Ingredients</h3>
+        <ul className="list-disc list-inside mb-4">
+          {recipe.ingredients.map((ing, index) => (
+            <li key={index}>
+              <Link href={`/recipes?ingredient=${ing.name}`}>{ing.name}</Link>
+              <span className="text-gray-400"> — {ing.measure}</span>
+            </li>
+          ))}
+        </ul>
+
+        <h3 className="text-lg font-semibold mb-2">Instructions</h3>
+        <div>
+          {recipe.instructions.split("\n").map((line, index) => (
+            <p key={index} className="mb-2 indent-2">
+              {line}
+            </p>
+          ))}
+        </div>
+      </main>
+
+      <Link href={`/recipes?category=${recipe.category}`}>
+        <aside className="px-4 py-2 m-4 border-1 border-gray-400 rounded-lg text-gray-400">
+          <h3 className="text-lg font-semibold mb-2">
+            Recipes from the same category
+          </h3>
+          <ul className="list-disc list-inside">
+            {recipiesFromCategory.map((r) => (
+              <li key={r.id}>{r.title}</li>
+            ))}
+          </ul>
+        </aside>
+      </Link>
     </div>
   );
 }
